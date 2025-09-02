2bb782009e46fb508ae6e06bb13a9bbdb0860279
Sevastian Caballero <115429070+Zylown@users.noreply.github.com>
2025-09-02 15:36:33 -0500

    Base 0 + Initialize React + TypeScript + Vite project
    Set up a new project with React, TypeScript, and Vite. Added initial configuration files including ESLint, TypeScript configs, Vite config, Tailwind CSS, and global styles. Updated README with setup and linting instructions. Added .gitignore for common Node and editor files.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-2xl">
+        Base 0 <b className="text-xs">Proyecto u</b>
+      </p>
+    </>
+  );
+}
+
+export default App;
