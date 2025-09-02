09a210bf44254414c7eff9657a85863e9eb1e715
Sevastian Caballero <115429070+Zylown@users.noreply.github.com>
2025-09-02 16:03:53 -0500

    Update dependencies and add color accent
    Updated package.json dependencies and added package-lock.json for reproducible installs. Modified src/App.tsx to improve layout and updated src/global.css for better styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import "./global.css";
+// import "./global.css";
 
 function App() {
   return (
     <>
       <p className="text-2xl">
-        Base 0 <b className="text-xs">Proyecto u</b>
+        Base 0{" "}
+        <b className="text-xs">
+          Proyecto <span className="text-accent font-light">u</span>
+        </b>
       </p>
+      <h1 className="text-accent">Texto morado</h1>
     </>
   );
 }
