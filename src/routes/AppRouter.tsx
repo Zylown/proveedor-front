1b37af53d6af16a8f28c18e65301faf8d238223d
Sevastian Caballero <115429070+Zylown@users.noreply.github.com>
2025-10-01 07:21:14 -0500

    Update imports for Order and Facturacion pages
    Changed imports for Order and Facturacion to reference their respective index files, ensuring correct module resolution and consistency with other page imports.

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -9,8 +9,8 @@ import Login from "../pages/Login";
 import Proveedores from "../pages/Proveedores";
 import Entregas from "../pages/Entregas";
 import DashboardLayoutRouter from "../layouts/DashboardLayoutRouter";
-import Order from "../pages/Order";
-import Facturacion from "../pages/Facturacion";
+import Order from "../pages/Order/index";
+import Facturacion from "../pages/Facturacion/index";
 
 const AppRouter = () => {
   return (
